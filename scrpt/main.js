4aff48e3af77724fa39c010d7367e7c779c655cf
sender <endernet@gmail.com>
2021-08-21 20:28:45 +0200

    github test

---
+++ scrpt/main.js
@@ -0,0 +1,9 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+const myHeading3 = document.querySelector('h3');
+myHeading3.textContent = 'SUPERNOVA H3 HEADING';
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
