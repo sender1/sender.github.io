3c2a46574130e433ad309553e87ff686bbf3a014
sender <endernet@gmail.com>
2021-08-21 22:48:11 +0200

    + change user-button; + change pic on click

--- scrpt/main.js
+++ scrpt/main.js
@@ -1,9 +1,40 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
 
 const myHeading3 = document.querySelector('h3');
 myHeading3.textContent = 'SUPERNOVA H3 HEADING';
 
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
+
+let myImage = document.querySelector('img');
+
+	myImage.onclick = function() {
+		let mySrc = myImage.getAttribute('src');
+		if(mySrc === 'img/image3-ant.png') {
+		  myImage.setAttribute('src','img/bioluminiszenz.jpg');
+		} else {
+		  myImage.setAttribute('src','img/image3-ant.png');
+		}
+	}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();        // Dialog nochmals anzeigen falls keine Name eingegeben wird: myname = null
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.textContent = 'Mozilla is cool, ' + myName;
+    }
 }
+  
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
